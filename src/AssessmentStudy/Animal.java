99d6260781996afb64146776d3a11ad7b029df60
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-13 22:06:15 -0600

    changes to study assessment

--- src/AssessmentStudy/Animal.java
+++ src/AssessmentStudy/Animal.java
@@ -1,27 +1,46 @@
 package AssessmentStudy;
 
+import java.util.ArrayList;
+
 //TODO: 3. Create an Animal class. It should have private instance properties for a name and species, and public getters and setters for both.
-public class Animal {
+public class Animal extends Assessment {
     public static void main(String[] args) {
+
+        Animal houseCat = new Animal();
+        houseCat.setName("Kitty"); //If not in Animal Class, .setName is relevant to call in other classes
+        houseCat.setSpecies("Felis catus");
+        houseCat.roar();
 
         Animal tiger = new Animal();
         tiger.name = "Tiger";
-
+        tiger.setSpecies("Feliz catuz");
         System.out.println(tiger.getName());
-        tiger.setName("Tigger");
         System.out.println(tiger.roar());
+
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(houseCat);
+        animals.add(tiger);
+        System.out.println(animals);
+
+//        Animal.catRemoval(animals);
+//        System.out.println(animals);
+
+        for (Animal animal : animals) {
+            System.out.println(animal.catRemoval(animals));
+        }
+
     }
 
-    //Private instances
+    //Private Instances
     private String name;
     private String species;
 
     //Getters
     public String getName() {
-        return this.name;
+        return name;
     }
     public String getSpecies() {
-        return this.species;
+        return species;
     }
 
     //Setters
@@ -34,7 +53,7 @@ public class Animal {
 
 //TODO: 4. Within your Animal class, define an instance method named roar. It should accept no arguments and return a string like "I am $NAME, hear me roar!" where $NAME is replaced with the value of the name property for that object.
     public String roar() {
-        return "I am " + getName() + ", hear me roar!";
+        return "I am " + name + ", hear me roar!";
     }
 
 }
--- src/AssessmentStudy/Assessment.java
+++ src/AssessmentStudy/Assessment.java
@@ -1,13 +1,17 @@
 package AssessmentStudy;
 
+import java.util.ArrayList;
+
 public class Assessment {
     public static void main(String[] args) {
+
+//        Animal.catRemoval(animals);
 
     }
 
     //TODO: 1. Create an Assessment class with a public static method named half. It should accept a number and return the number divided by two.
     public static int half(int number) {
-        return number = number / 2;
+        return number / 2;
     }
 
     //TODO: 2. Within your Assessment class create a public static method named shout. It should accept a string and return the same string in all upper case with three exclamation marks added to the end of it.
@@ -15,5 +19,13 @@ public class Assessment {
         return string.toUpperCase() + "!!!";
     }
 
-
+//TODO: 5. Within your Assessment class create a public static method named removeCats. It should accept a list of animal objects and return a list of animal objects where any animal object with a species of "Felis catus" is removed.
+    public static ArrayList<Animal>catRemoval(ArrayList<Animal> animals) {
+        for (int i = 0; i < animals.size(); i++) {
+            if (animals.get(i).getSpecies().equals("Felis catus")) {
+                animals.remove(i);
+            }
+        }
+        return animals;
+    }
 }

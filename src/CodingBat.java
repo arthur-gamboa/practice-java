8182c9e3beb01ad554aa75a2c81184e1467626ca
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-15 21:39:15 -0600

    continuing problem solving

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -82,6 +82,8 @@ public class CodingBat {
 //                (Math.abs(200 - n) <= 10));
 //    }
 
+//TODO: 8. Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
 
 
 }

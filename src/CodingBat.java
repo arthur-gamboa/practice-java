b6b9b72afbc69f98dd6f5bc302d33d778b87d668
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-16 20:12:16 -0600

    continuing java practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -84,6 +84,8 @@ public class CodingBat {
 
 //TODO: 8. Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
 
+    public boolean posNeg(int a, int b, boolean negative) {
 
+    }
 
 }

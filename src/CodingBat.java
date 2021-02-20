2e882809bee9d52cef7728d425910d0b2d27c97c
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-19 23:47:24 -0600

    continuing problem solving

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -115,4 +115,16 @@ public class CodingBat {
         return front + back;
     }
 
+//TODO: 11. Given a string, return a new string where the first and last chars have been exchanged.
+
+    public String frontBack(String str) {
+        if (str.length() <= 1) return str;
+
+        String mid = str.substring(1, str.length()-1);
+
+        // last + mid + first
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    }
+
+
 }

71925e103db176ee7a90cc68c0206c5f454f35a1
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-19 23:48:30 -0600

    continuing problem solving

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -126,5 +126,19 @@ public class CodingBat {
         return str.charAt(str.length()-1) + mid + str.charAt(0);
     }
 
+//TODO: 12. Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+    public String front3(String str) {
+        String front;
+
+        if (str.length() >= 3) {
+            front = str.substring(0, 3);
+        }
+        else {
+            front = str;
+        }
+
+        return front + front + front;
+    }
 
 }

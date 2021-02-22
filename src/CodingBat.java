4dada01a1ab2604bedbd63f28382e1368198397a
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-02-21 19:49:20 -0600

    continuing problem solving

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -147,6 +147,12 @@ public class CodingBat {
         return replace + str + replace;
     }
 
+//TODO: 14. Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator.
+
+    public boolean or35(int n) {
+        return (n % 3 == 0) || (n % 5 == 0);
+    }
+
 
 
 }

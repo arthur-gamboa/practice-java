77f87cbcdd2f8a57b786b7c1a94ba0b7e320cd17
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-15 22:58:31 -0500

    continuing codingbat problems

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -269,4 +269,18 @@ public class CodingBat {
     }
         // extra review on this problem
 
+//TODO: 27. Return true if the given string contains between 1 and 3 'e' chars.
+
+    public boolean stringE(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e')
+                count = count + 1;
+        }
+        return (count >= 1 && count <= 3);
+    }
+
+
+
 }

bfc6dff35f1ab2190557d6912e16b5d99b81e46d
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-17 13:24:11 -0500

    continuing codingbat problems

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -281,6 +281,21 @@ public class CodingBat {
         return (count >= 1 && count <= 3);
     }
 
+//TODO: 28. Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
 
+    public boolean lastDigit(int x, int y) {
+        return (x % 10 == y % 10);
+    }
+
+//TODO: 29. Given a string, return a new string where the last 3 chars are now in upper case. If the string has less than 3 chars, uppercase whatever is there. Note that str.toUpperCase() returns the uppercase version of a string.
+
+    public String endUp(String str) {
+        if (str.length() <= 3) return str.toUpperCase();
+        int cut = str.length() - 3;
+        String front = str.substring(0, cut);
+        String back  = str.substring(cut);
+
+        return front + back.toUpperCase();
+    }
 
 }

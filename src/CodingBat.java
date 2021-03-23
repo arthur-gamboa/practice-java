9f091c5da1f7595b8524bc020cb21df1216b4f82
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-22 20:32:16 -0500

    continuing codingbat practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -368,4 +368,15 @@ public class CodingBat {
         return result.toString();
     }
 
+//TODO: 37. Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+    public String stringSplosion(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            result.append(str.substring(0, i + 1));
+        }
+        return result.toString();
+    }
+
 }

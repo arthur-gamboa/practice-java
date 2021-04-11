6cdf06c6af07c7a2fdd6ed7dcc9f8c0935972e0d
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-04-10 23:19:12 -0500

    continuing codingbat practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -429,4 +429,20 @@ public class CodingBat {
         return false;
     }
 
+//TODO: 42. Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+
+    public int stringMatch(String a, String b) {
+        int len = Math.min(a.length(), b.length());
+        int count = 0;
+
+        for (int i=0; i<len-1; i++) {
+            String aSub = a.substring(i, i + 2);
+            String bSub = b.substring(i, i + 2);
+            if (aSub.equals(bSub)) {
+                return count++;
+            }
+        }
+        return count;
+    }
+
 }

b19626530d6927b63e93a520283c2b5263900e18
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-05-17 23:55:08 -0500

    continuing codingbat practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -445,4 +445,20 @@ public class CodingBat {
         return count;
     }
 
+//TODO: 43. Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+
+    public String altPairs(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i=0; i<str.length(); i += 4) {
+            int end = i + 2;
+            if (end > str.length()) {
+                end = str.length();
+            }
+            result.append(str.substring(i, end));
+        }
+
+        return result.toString();
+    }
+
 }

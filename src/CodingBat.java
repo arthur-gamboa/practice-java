068d4d48065d9260791b1a9988c14d4d43bb7119
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-05-18 23:34:31 -0500

    continuing codingbat practice

--- src/CodingBat.java
+++ src/CodingBat.java
@@ -461,4 +461,20 @@ public class CodingBat {
         return result.toString();
     }
 
+//TODO: 44. Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
+
+    public String stringYak(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
+                i =  i + 2;
+            } else {
+                result.append(str.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+
 }
